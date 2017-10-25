ef7ba66d3952ba3386bcf615310dfe247c0e6e76
Yevgeniy Valeyev <webmaster.vea@gmail.com>
2017-10-25 09:36:58 +0200

    https

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,7 +6,7 @@ export interface AppConfig {
 
 export const APP_CONFIG_DATA: AppConfig = {
   // usersApi: 'http://test-api.javascript.ru/v1/ujeen/users'
-  usersApi: 'http://test-api.javascript.ru/v1/ujeen/users'
+  usersApi: 'https://test-api.javascript.ru/v1/ujeen/users'
 }
 
 export const APP_CONFIG = new InjectionToken<AppConfig>('app.config');
